f34fb8468e828bec8953a7dda2a13f096d911b4a
K M Amaz Uddin Shaon <k.m.shaonred@gmail.com>
2022-06-26 23:52:53 +0600

    Icaon Add in Router List

--- app/navigation/Navbar.js
+++ app/navigation/Navbar.js
@@ -3,14 +3,41 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import AduioList from "../screen/AduioList";
 import PlayList from "../screen/PalyList";
 import Player from "../screen/Player";
+import { Ionicons } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 const Navbar = () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Aduio List" component={AduioList} />
-      <Tab.Screen name="Player" component={Player} />
-      <Tab.Screen name="Play List" component={PlayList} />
+      <Tab.Screen
+        name="Aduio List"
+        component={AduioList}
+        options={{
+          tabBarIcon: () => {
+            return <Ionicons name="headset" size={24} color="black" />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Player"
+        component={Player}
+        options={{
+          tabBarIcon: () => {
+            return <FontAwesome name="play" size={24} color="black" />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Play List"
+        component={PlayList}
+        options={{
+          tabBarIcon: () => {
+            return <AntDesign name="heart" size={24} color="black" />;
+          },
+        }}
+      />
     </Tab.Navigator>
   );
 };
