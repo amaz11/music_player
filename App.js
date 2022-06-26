457636fe64eb63fef605af0c339335232eda78c9
K M Amaz Uddin Shaon <k.m.shaonred@gmail.com>
2022-06-26 23:38:06 +0600

    React native route system Implement

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import Navbar from "./app/navigation/Navbar";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Music App</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Navbar />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
